c8e1fccddaa5b55f42cb426d39b26e609e263240
McKinley <caiohudson.red4@hotmail.com>
2024-03-28 18:51:23 -0300

    Cards implemented

---
+++ Horusboard/Assets/Scripts/CardSystem/CardBehaviour.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CardBehaviour : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] 
+    private CardData cardData;
+    
+    [SerializeField]
+    private RectTransform cardTransform;
+    
+    [Header("Card Image Displays")]
+    [SerializeField] 
+    private Image cardBG;
+    [SerializeField]
+    private Image cardIcon;
+
+    [Header("Card Text Displays")]
+    [SerializeField] 
+    private RectTransform cardDescriptionBox;
+    [SerializeField] 
+    private TextMeshProUGUI cardDescriptionDisplay;
+    
+    private Vector2 cardDescBox_Pos;
+    
+    public bool selected;
+    public void SetCardData(CardData cardData)
+    {
+        this.cardData = cardData;
+
+        cardBG.sprite = cardData.cardBackground;
+        cardIcon.sprite = cardData.cardIcon;
+        cardDescriptionDisplay.text = cardData.cardDescription;
+
+        Color bgColor = Color.gray;
+        
+        switch (cardData.cardType)
+        {
+            case CardType.WATER:
+                bgColor = Color.blue;
+                break;
+            case CardType.EARTH:
+                bgColor = new Color(1f, .6f, 0f);
+                break;
+            case CardType.FIRE:
+                bgColor = Color.red;
+                break;
+            case CardType.AIR:
+                bgColor = Color.cyan;
+                break;
+        }
+
+        cardBG.color = bgColor;
+    }
+    
+    void Start()
+    {
+        if (cardTransform == null)
+            cardTransform = GetComponent<RectTransform>();
+
+        cardDescBox_Pos = cardDescriptionBox.anchoredPosition;
+        
+        SetCardData(cardData);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        selected = !selected;
+
+        if(selected)
+            cardDescriptionBox.DOAnchorPosX(-cardTransform.sizeDelta.x, .2f).SetRelative(true);
+        else
+            cardDescriptionBox.DOAnchorPos(cardDescBox_Pos, .2f);
+        
+
+    }
+}
---
+++ Horusboard/Assets/Scripts/CardSystem/CardData.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+
+public enum CardType
+{
+    WATER,
+    EARTH,
+    FIRE,
+    AIR
+}
+[CreateAssetMenu(menuName = "Card")]
+public class CardData : ScriptableObject
+{
+    public string cardName;
+    
+    public Sprite cardIcon;
+    public Sprite cardBackground;
+    
+    [TextArea]
+    public string cardDescription;
+    
+    [Header("Damage Properties")] 
+    public int damageBuff; // Buffs user's Attack (Prioritizes User)
+    public int damageDebuff;
+    
+    [Header("Defense Properties")]
+    public int defenseBuff; // Buffs user's Defense (Prioritizes User)
+    public int defenseDebuff; // Lowers Defense
+
+    [Header("Healing Properties")] 
+    public int cureBuff;
+    public bool cureDebuff;
+
+    [SerializeField] 
+    public CardType cardType;
+    
+}

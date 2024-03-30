2375e7828aaba4eadb08fca0f312c956ec4080fd
McKinley <caiohudson.red4@hotmail.com>
2024-03-30 03:28:09 -0300

    adding fill and adjustments

--- Horusboard/Assets/Scripts/Systems/MenuManager.cs
+++ Horusboard/Assets/Scripts/Systems/MenuManager.cs
@@ -1,13 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-
     [SerializeField] 
     private ChangeSceneSystem changeSystem;
-    
+
+    [SerializeField]
+    private Sound mainTheme;
+    private void Start()
+    {
+        AudioManager.Instance.Play(mainTheme);
+    }
+
     public void StartGame()
     {
         changeSystem.LoadScene(1);
